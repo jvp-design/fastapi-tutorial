58a2ed56e614fa46c2bf8dc1fcb28066f55408d0
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-02-01 17:21:21 -0500

    feat: initial project setup

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "hello world"}
+
+
+@app.post("/")
+async def post():
+    return {"message": "hello from the post route"}
+
+
+@app.put("/")
+async def put():
+    return {"message": "hello from the put route"}
