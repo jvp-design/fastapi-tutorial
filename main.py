92d835fad389386fd1fb150af0da9c3d538ff2d3
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-02-03 15:11:35 -0500

    feat: path parameters

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -16,3 +18,37 @@ async def post():
 @app.put("/")
 async def put():
     return {"message": "hello from the put route"}
+
+
+@app.get("/users")
+async def list_users():
+    return {"message": "list users route"}
+
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"Message": "this is the current user"}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"user_id": user_id}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweet things",
+        }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
