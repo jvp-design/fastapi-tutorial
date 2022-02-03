e7b824940408dc44b3753190d24e9634b989722b
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-02-03 15:29:12 -0500

    feat: query parameters

--- main.py
+++ main.py
@@ -52,3 +52,43 @@ async def get_food(food_name: FoodEnum):
             "message": "you are still healthy, but like sweet things",
         }
     return {"food_name": food_name, "message": "i like chocolate milk"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(
+    item_id: str, sample_query_param: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+        return item
