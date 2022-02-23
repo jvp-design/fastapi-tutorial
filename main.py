8fbfe93b7f8b47be680a0a8da80a99a435973679
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-02-23 17:42:58 -0500

    feat: declare request example data

--- main.py
+++ main.py
@@ -202,45 +202,93 @@ app = FastAPI()
 #     return results
 
 ## Part 9 -> Body - Nested Models
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: set[str] = []
+#     image: list[Image] | None = None
+#
+#
+# class Offer(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     items: list[Item]
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+#
+#
+# @app.post("/offers")
+# async def create_offer(offer: Offer = Body(..., embed=True)):
+#     return offer
+#
+#
+# @app.post("/images/multiple")
+# async def create_multiple_images(images: list[Image]):
+#     return images
+#
+#
+# @app.post("/blah")
+# async def create_some_blahs(blahs: dict[int, float]):
+#     return blahs
 
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
+## Part 10 - Declare Request Example Data
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
-    tags: set[str] = []
-    image: list[Image] | None = None
 
-
-class Offer(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    items: list[Item]
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "name": "Foo",
+    #             "description": "A very nice Item",
+    #             "price": 16.25,
+    #             "tax": 1.67,
+    #         }
+    #     }
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(
+    item_id: int,
+    item: Item = Body(
+        ...,
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A __normal__ item works _correctly_",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 16.25,
+                    "tax": 1.67,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {"name": "Bar", "price": "16.25"},
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "description": "Hello youtubers",
+                "value": {"name": "Baz", "price": "sixteen point two five"},
+            },
+        },
+    ),
+):
     results = {"item_id": item_id, "item": item}
     return results
-
-
-@app.post("/offers")
-async def create_offer(offer: Offer = Body(..., embed=True)):
-    return offer
-
-
-@app.post("/images/multiple")
-async def create_multiple_images(images: list[Image]):
-    return images
-
-
-@app.post("/blah")
-async def create_some_blahs(blahs: dict[int, float]):
-    return blahs
