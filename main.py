8c0c1869745e90620e3e039d93e5d5e4071e0245
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-02-24 17:24:05 -0500

    feat: cookie and header parameters

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime, time, timedelta
 from enum import Enum
 from uuid import UUID
 
-from fastapi import Body, FastAPI, Query, Path
+from fastapi import Body, FastAPI, Query, Path, Cookie, Header
 from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
@@ -296,22 +296,39 @@ app = FastAPI()
 #     return results
 
 ## Part 11 - Extra Data Types
-@app.put("/items/{item_id}")
+# @app.put("/items/{item_id}")
+# async def read_items(
+#     item_id: UUID,
+#     start_date: datetime | None = Body(None),
+#     end_date: datetime | None = Body(None),
+#     repeat_at: time | None = Body(None),
+#     process_after: timedelta | None = Body(None),
+# ):
+#     start_process = start_date + process_after
+#     duration = end_date - start_process
+#     return {
+#         "item_id": item_id,
+#         "start_date": start_date,
+#         "end_date": end_date,
+#         "repeat_at": repeat_at,
+#         "process_after": process_after,
+#         "start_process": start_process,
+#         "duration": duration,
+#     }
+
+## Part 12 - Cookie and Header Parameters
+@app.get("/items")
 async def read_items(
-    item_id: UUID,
-    start_date: datetime | None = Body(None),
-    end_date: datetime | None = Body(None),
-    repeat_at: time | None = Body(None),
-    process_after: timedelta | None = Body(None),
+    cookie_id: str | None = Cookie(None),
+    accept_encoding: str | None = Header(None),
+    sec_ch_ua: str | None = Header(None),
+    user_agent: str | None = Header(None),
+    x_token: list[str] | None = Header(None),
 ):
-    start_process = start_date + process_after
-    duration = end_date - start_process
     return {
-        "item_id": item_id,
-        "start_date": start_date,
-        "end_date": end_date,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
+        "cookie_id": cookie_id,
+        "Accept-Encoding": accept_encoding,
+        "sec-ch-ua": sec_ch_ua,
+        "User-Agent": user_agent,
+        "X-Token values": x_token,
     }
