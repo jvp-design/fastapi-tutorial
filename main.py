126f033f94373789275bf8b558c2ee582446578e
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-03-08 17:20:39 -0500

    feat: request files

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from enum import Enum
 from typing import Literal, Union
 from uuid import UUID
 
-from fastapi import Body, FastAPI, Query, Path, Cookie, Header, status, Form
+from fastapi import (
+    Body,
+    FastAPI,
+    Query,
+    Path,
+    Cookie,
+    Header,
+    status,
+    Form,
+    File,
+    UploadFile,
+)
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -494,13 +506,44 @@ app = FastAPI()
 #     return {"hello": "world"}
 
 ## Part 16 - Form Fields
-@app.post("/login/")
-async def login(username: str = Form(...), password: str = Body(...)):
-    print("password", password)
-    return {"username": username}
+# @app.post("/login/")
+# async def login(username: str = Form(...), password: str = Body(...)):
+#     print("password", password)
+#     return {"username": username}
+#
+#
+# @app.post("/login-json/")
+# async def login_json(username: str = Body(...), password: str = Body(...)):
+#     print("password", password)
+#     return {"username": username}
+
+## Part 17 - Request Files
+@app.post("/files/")
+async def create_file(
+    files: list[bytes] = File(..., description="A file read as bytes")
+):
+    return {"file_sizes": [len(file) for file in files]}
 
 
-@app.post("/login-json/")
-async def login_json(username: str = Body(...), password: str = Body(...)):
-    print("password", password)
-    return {"username": username}
+@app.post("/uploadfile/")
+async def create_upload_file(
+    files: list[UploadFile] = File(..., description="A file read as UploadFile")
+):
+    return {"filename": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
