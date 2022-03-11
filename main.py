eedb9619be8cd8e90e35a8eda2261410de347d97
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-03-11 16:40:43 -0500

    feat: request forms and files

--- main.py
+++ main.py
@@ -518,32 +518,47 @@ app = FastAPI()
 #     return {"username": username}
 
 ## Part 17 - Request Files
+# @app.post("/files/")
+# async def create_file(
+#     files: list[bytes] = File(..., description="A file read as bytes")
+# ):
+#     return {"file_sizes": [len(file) for file in files]}
+#
+#
+# @app.post("/uploadfile/")
+# async def create_upload_file(
+#     files: list[UploadFile] = File(..., description="A file read as UploadFile")
+# ):
+#     return {"filename": [file.filename for file in files]}
+#
+#
+# @app.get("/")
+# async def main():
+#     content = """
+# <body>
+# <form action="/files/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# </body>
+#     """
+#     return HTMLResponse(content=content)
+
+## Part 18 - Request Forms and Files
 @app.post("/files/")
 async def create_file(
-    files: list[bytes] = File(..., description="A file read as bytes")
+    file: bytes = File(...),
+    fileb: UploadFile = File(...),
+    token: str = Form(...),
+    hello: str = Body(...),
 ):
-    return {"file_sizes": [len(file) for file in files]}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(
-    files: list[UploadFile] = File(..., description="A file read as UploadFile")
-):
-    return {"filename": [file.filename for file in files]}
-
-
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+        "hello": hello,
+    }
