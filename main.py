3b768629e6dc9d8a40915cedf4922cb95bbe74d2
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-04-27 16:49:43 -0400

    feat: json compatible encoder and body - updates

--- main.py
+++ main.py
@@ -730,7 +730,6 @@ items = {
         "description": "The bartenders",
         "price": 62,
         "tax": 20.2,
-        "tags": ["1", "2", "3"],
     },
     "baz": {
         "name": "Baz",
@@ -744,22 +743,24 @@ items = {
 
 @app.get("/items/{item_id}", response_model=Item)
 async def read_item(item_id: str):
-    return items[item_id]
+    return items.get(item_id)
 
 
 @app.put("/items/{item_id}", response_model=Item)
-async def update_item(item_id: str, item: Item):
+def update_item(item_id: str, item: Item):
     update_item_encoded = jsonable_encoder(item)
     items[item_id] = update_item_encoded
     return update_item_encoded
 
 
 @app.patch("/items/{item_id}", response_model=Item)
-async def patch_item(item_id: str, item: Item):
-    stored_item_data = items[item_id]
-    stored_item_model = Item(**stored_item_data)
+def patch_item(item_id: str, item: Item):
+    stored_item_data = items.get(item_id)
+    if stored_item_data is not None:
+        stored_item_model = Item(**stored_item_data)
+    else:
+        stored_item_model = Item()
     update_data = item.dict(exclude_unset=True)
-    print(update_data)
     updated_item = stored_item_model.copy(update=update_data)
     items[item_id] = jsonable_encoder(updated_item)
     return updated_item
