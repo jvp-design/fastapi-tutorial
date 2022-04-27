9387da86751a52c46dd84093d160c2c4604a1484
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-04-27 17:03:37 -0400

    feat: dependencies intro

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from uuid import UUID
 
 from fastapi import (
     Body,
+    Depends,
     FastAPI,
     Query,
     Path,
@@ -715,52 +716,72 @@ app = FastAPI()
 #     return [{"item_id": "Foo"}]
 
 ## Part 21a - JSON Compatible Encoder
-class Item(BaseModel):
-    name: str | None = None
-    description: str | None = None
-    price: float | None = None
-    tax: float = 10.5
-    tags: list[str] = []
+# class Item(BaseModel):
+#     name: str | None = None
+#     description: str | None = None
+#     price: float | None = None
+#     tax: float = 10.5
+#     tags: list[str] = []
+#
+#
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {
+#         "name": "Bar",
+#         "description": "The bartenders",
+#         "price": 62,
+#         "tax": 20.2,
+#     },
+#     "baz": {
+#         "name": "Baz",
+#         "description": None,
+#         "price": 50.2,
+#         "tax": 10.5,
+#         "tags": [],
+#     },
+# }
+#
+#
+# @app.get("/items/{item_id}", response_model=Item)
+# async def read_item(item_id: str):
+#     return items.get(item_id)
+#
+#
+# @app.put("/items/{item_id}", response_model=Item)
+# def update_item(item_id: str, item: Item):
+#     update_item_encoded = jsonable_encoder(item)
+#     items[item_id] = update_item_encoded
+#     return update_item_encoded
+#
+#
+# @app.patch("/items/{item_id}", response_model=Item)
+# def patch_item(item_id: str, item: Item):
+#     stored_item_data = items.get(item_id)
+#     if stored_item_data is not None:
+#         stored_item_model = Item(**stored_item_data)
+#     else:
+#         stored_item_model = Item()
+#     update_data = item.dict(exclude_unset=True)
+#     updated_item = stored_item_model.copy(update=update_data)
+#     items[item_id] = jsonable_encoder(updated_item)
+#     return updated_item
+
+## Part 22 - Dependencies Intro
+async def hello():
+    return "world"
 
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {
-        "name": "Bar",
-        "description": "The bartenders",
-        "price": 62,
-        "tax": 20.2,
-    },
-    "baz": {
-        "name": "Baz",
-        "description": None,
-        "price": 50.2,
-        "tax": 10.5,
-        "tags": [],
-    },
-}
+async def common_parameters(
+    q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
+):
+    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
 
 
-@app.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: str):
-    return items.get(item_id)
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
 
 
-@app.put("/items/{item_id}", response_model=Item)
-def update_item(item_id: str, item: Item):
-    update_item_encoded = jsonable_encoder(item)
-    items[item_id] = update_item_encoded
-    return update_item_encoded
-
-
-@app.patch("/items/{item_id}", response_model=Item)
-def patch_item(item_id: str, item: Item):
-    stored_item_data = items.get(item_id)
-    if stored_item_data is not None:
-        stored_item_model = Item(**stored_item_data)
-    else:
-        stored_item_model = Item()
-    update_data = item.dict(exclude_unset=True)
-    updated_item = stored_item_model.copy(update=update_data)
-    items[item_id] = jsonable_encoder(updated_item)
-    return updated_item
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
