350e9af18d3a430610b3fa59fd3b46387c1c6857
jeffpohlmeyer <jeffvp@protonmail.ch>
2022-05-04 17:26:06 -0400

    feat: classes as dependencies

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-
 # @app.get("/")
 # async def root():
 #     return {"message": "hello world"}
@@ -767,21 +766,41 @@ app = FastAPI()
 #     return updated_item
 
 ## Part 22 - Dependencies Intro
-async def hello():
-    return "world"
+# async def hello():
+#     return "world"
+#
+#
+# async def common_parameters(
+#     q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
+# ):
+#     return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+#
+#
+# @app.get("/items/")
+# async def read_items(commons: dict = Depends(common_parameters)):
+#     return commons
+#
+#
+# @app.get("/users/")
+# async def read_users(commons: dict = Depends(common_parameters)):
+#     return commons
+
+## Part 23 - Classes as Dependencies
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-async def common_parameters(
-    q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
-):
-    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
 
-@app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: dict = Depends(common_parameters)):
-    return commons
+@app.get("/items/{item_id}")
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
