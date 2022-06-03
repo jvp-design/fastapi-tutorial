89da76f8528abd1d7bb0794eb21caa212aede634
jeffpohlmeyer <jeffvp@protonmail.ch>
2022-06-03 15:00:25 -0400

    feat: SQL Databases

--- main.py
+++ main.py
@@ -1065,19 +1065,19 @@ app = FastAPI()
 #     return [{"item_id": "Foo", "owner": current_user.username}]
 
 ## Part 28 - Middleware and CORS
-class MyMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        start_time = time.time()
-        response = await call_next(request)
-        process_time = time.time() - start_time
-        response.headers["X-Process-Time"] = str(process_time)
-        return response
-
-
-origins = ["http://localhost:8000", "http://localhost:3000"]
-app.add_middleware(MyMiddleware)
-app.add_middleware(CORSMiddleware, allow_origins=origins)
-
-@app.get("/blah")
-async def blah():
-    return {"hello": "world"}
+# class MyMiddleware(BaseHTTPMiddleware):
+#     async def dispatch(self, request: Request, call_next):
+#         start_time = time.time()
+#         response = await call_next(request)
+#         process_time = time.time() - start_time
+#         response.headers["X-Process-Time"] = str(process_time)
+#         return response
+#
+#
+# origins = ["http://localhost:8000", "http://localhost:3000"]
+# app.add_middleware(MyMiddleware)
+# app.add_middleware(CORSMiddleware, allow_origins=origins)
+#
+# @app.get("/blah")
+# async def blah():
+#     return {"hello": "world"}
