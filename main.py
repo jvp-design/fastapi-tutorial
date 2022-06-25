681ec99e2bf1b56daefae591a01fb01922aa94c0
jeffpohlmeyer <jeffvp@protonmail.ch>
2022-06-25 08:18:28 -0400

    fix: reverting background tasks from incorrect branch

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ from fastapi import (
     UploadFile,
     HTTPException,
     Request,
-    BackgroundTasks,
 )
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
@@ -1082,40 +1081,3 @@ app = FastAPI()
 # @app.get("/blah")
 # async def blah():
 #     return {"hello": "world"}
-
-## Part 31 - Background Tasks
-# import time
-
-
-# def write_notification(email: str, message=""):
-#     with open("log.txt", mode="w") as email_file:
-#         content = f"notification for {email}: {message}"
-#         time.sleep(5)
-#         email_file.write(content)
-#
-#
-# @app.post("/send-notification/{email}", status_code=202)
-# async def send_notification(email: str, background_tasks: BackgroundTasks):
-#     background_tasks.add_task(write_notification, email, message="some notification")
-#     return {"message": "Notification sent in the background"}
-
-
-def write_log(message: str):
-    with open("log.txt", mode="a") as log:
-        log.write(message)
-
-
-def get_query(background_tasks: BackgroundTasks, q: str | None = None):
-    if q:
-        message = f"found query: {q}\n"
-        background_tasks.add_task(write_log, message)
-    return q
-
-
-@app.post("/send-notification/{email}")
-async def send_notification(
-    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
-):
-    message = f"message to {email}\n"
-    background_tasks.add_task(write_log, message)
-    return {"message": "Message sent", "query": q}
